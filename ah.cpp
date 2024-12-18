cbdaef39565d27ffd1e55b3a5bcb750fee98b1db
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-12-18 08:18:31 -0500

    Add files via upload

---
+++ ah.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+#include <ctime>
+
+int main(){
+    //random number between 1 and 10
+    cout << (rand()%10)+1 << endl;
+    
+    //rounding
+    double doub = 1.853241234123;
+    cout << round(doub*100)/100 << endl;
+
+}

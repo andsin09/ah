9adc8f1ebf2483430050be489a650f4221379db2
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-12-18 08:19:55 -0500

    Update ah.cpp

--- ah.cpp
+++ ah.cpp
@@ -11,4 +11,6 @@ int main(){
     double doub = 1.853241234123;
     cout << round(doub*100)/100 << endl;
 
+    //Question: Why do we need time for random numbers?
+    //Answer: Because time can be used as a seed for random numbers to reduce the size of the random numbers.
 }

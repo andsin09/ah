54764c0168e01f032c88c4567947c1aa6f14f111
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-12-18 08:52:01 -0500

    Update ah.cpp

--- ah.cpp
+++ ah.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 int main(){
     //random number between 1 and 10
+    srand(time(0));
     cout << (rand()%10)+1 << endl;
     
     //rounding
     double doub = 1.853241234123;
     cout << round(doub*100)/100 << endl;
 
-    //Question: Why do we need time for random numbers?
-    //Answer: Because time can be used as a seed for random numbers to reduce the size of the random numbers.
 }
